79ba593a9a723350bcb65c59379c402f938b894c
justglidez <arcglidez@gmail.com>
2025-07-25 18:46:34 +0100

    Delete AAYPL WEB directory

--- AAYPL WEB/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/api.ts
-
-export async function fetcher<T>(input: RequestInfo, init?: RequestInit): Promise<T> {
-  const res = await fetch(input, init);
-  if (!res.ok) {
-    throw new Error(`API error: ${res.statusText}`);
-  }
-  return res.json();
-}
